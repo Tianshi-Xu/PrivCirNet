6cfd51c6b74168e69a61518e42eac659bda9bc3b
1907FB <994876761@qq.com>
2024-04-29 11:28:16 +0800

    .

--- train.sh
+++ train.sh
@@ -4,9 +4,9 @@ CUDA_VISIBLE_DEVICES=5 python train_cir.py -c configs/datasets/MBV2/cifar100_fix
 CUDA_VISIBLE_DEVICES=6 python train_cir.py -c configs/datasets/MBV2/tiny_fix.yml --model tiny_cir_mobilenetv2 /home/xts/code/dataset/tiny-imagenet-200
 
 # vit fix
-CUDA_VISIBLE_DEVICES=2 python train_cir.py -c configs/datasets/ViT/cifar10_fix.yml --model vit_7_4_32 /home/xts/code/dataset/cifar10/
-CUDA_VISIBLE_DEVICES=1 python train_cir.py -c configs/datasets/ViT/cifar100_fix.yml --model vit_7_4_32_c100 /home/xts/code/dataset/cifar100/
-CUDA_VISIBLE_DEVICES=1 python train_cir.py -c configs/datasets/ViT/tiny_fix.yml --model vit_9_12_64 /home/xts/code/dataset/tiny-imagenet-200/
+CUDA_VISIBLE_DEVICES=6 python train_cir.py -c configs/datasets/ViT/cifar10_fix.yml --model vit_7_4_32 /home/xts/code/dataset/cifar10/
+CUDA_VISIBLE_DEVICES=6 python train_cir.py -c configs/datasets/ViT/cifar100_fix.yml --model vit_7_4_32_c100 /home/xts/code/dataset/cifar100/
+CUDA_VISIBLE_DEVICES=6 python train_cir.py -c configs/datasets/ViT/tiny_fix.yml --model vit_9_12_64 /home/xts/code/dataset/tiny-imagenet-200/
 
 # mbv2 SpENCNN, pruning
 CUDA_VISIBLE_DEVICES=7 python train_prune.py -c configs/datasets/Prune/cifar10.yml --model c10_prune_mobilenetv2 /home/xts/code/dataset/cifar10/
