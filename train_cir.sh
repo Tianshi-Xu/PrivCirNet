7c78dcad79869cca23fa2d85ece1f44c3d23e9f6
1907FB <994876761@qq.com>
2024-05-08 21:02:01 +0800

    cvt

--- train_cir.sh
+++ train_cir.sh
@@ -15,6 +15,6 @@ CUDA_VISIBLE_DEVICES=6 python train_cir.py -c configs/datasets/ViT/tiny_fix.yml 
 CUDA_VISIBLE_DEVICES=4 python train_cir.py -c configs/datasets/ViT/tiny_fix_288.yml --model vit_9_12_64_288 /home/xts/code/dataset/tiny-imagenet-200/
 
 # cvt fix
-CUDA_VISIBLE_DEVICES=4 python train_cir.py -c configs/datasets/CVT/cifar10_fix.yml --model cvt_7_4_32 /home/xts/code/dataset/cifar10/
-CUDA_VISIBLE_DEVICES=5 python train_cir.py -c configs/datasets/CVT/cifar100_fix.yml --model cvt_7_4_32_c100 /home/xts/code/dataset/cifar100/
-CUDA_VISIBLE_DEVICES=3 python train_cir.py -c configs/datasets/CVT/tiny_fix.yml --model cvt_9_12_64 /home/xts/code/dataset/tiny-imagenet-200/
+CUDA_VISIBLE_DEVICES=6 python train_cir.py -c configs/datasets/CVT/cifar10_fix.yml --model cvt_7_4_32 /home/xts/code/dataset/cifar10/
+CUDA_VISIBLE_DEVICES=7 python train_cir.py -c configs/datasets/CVT/cifar100_fix.yml --model cvt_7_4_32_c100 /home/xts/code/dataset/cifar100/
+CUDA_VISIBLE_DEVICES=7 python train_cir.py -c configs/datasets/CVT/tiny_fix.yml --model cvt_9_12_64 /home/xts/code/dataset/tiny-imagenet-200/
