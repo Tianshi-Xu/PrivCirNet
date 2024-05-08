1f19ca70274b9b8c36d2d5b05f7c9503f4943602
1907FB <994876761@qq.com>
2024-05-08 21:08:23 +0800

    .

--- CirILP.py
+++ CirILP.py
@@ -652,7 +652,7 @@ def main():
     if args.use_kd:    
         train_loss_fn_kd = KLLossSoft().cuda()
     validate_loss_fn = nn.CrossEntropyLoss().cuda()
-
+    eval_metrics = validate(model, loader_eval, validate_loss_fn, args, amp_autocast=amp_autocast)
     if args.initial_checkpoint != "":
         _logger.info("Verifying initial model in training dataset")
         # sample in the training set!
@@ -738,7 +738,7 @@ def ILP(args,test_loader,model):
         variable[f"b16_{i}"] = LpVariable(f"b16_{i}", 0, 1, cat=LpInteger)
         # variable[f"b32_{i}"] = LpVariable(f"b32_{i}", 0, 1, cat=LpInteger)
     prob = LpProblem("Block_size", LpMinimize)
-    prob += sum(variable[f"b1_{i}"]*latency_weights_b1[i] +variable[f"b2_{i}"]*latency_weights_b2[i] + variable[f"b4_{i}"]*latency_weights_b4[i] +variable[f"b8_{i}"]*latency_weights_b8[i] +variable[f"b16_{i}"]*latency_weights_b16[i] for i in range(num_variable))-origin_latency*1.2 <= 0.01
+    prob += sum(variable[f"b1_{i}"]*latency_weights_b1[i] +variable[f"b2_{i}"]*latency_weights_b2[i] + variable[f"b4_{i}"]*latency_weights_b4[i] +variable[f"b8_{i}"]*latency_weights_b8[i] +variable[f"b16_{i}"]*latency_weights_b16[i] for i in range(num_variable))-origin_latency <= 0.01
 
     
     #one layer only have one blocksize
@@ -792,7 +792,7 @@ def ILP(args,test_loader,model):
             idx += 1
     _logger.info("result:"+str(result))
     _logger.info("origin_latency:"+str(origin_latency))
-    _logger.info("current_latency-origin_latency:"+str(current_latency-origin_latency*1.2))
+    _logger.info("current_latency-origin_latency:"+str(current_latency-origin_latency))
     
 
 # sample on training dataset and get gradients    
@@ -819,6 +819,11 @@ def train_one_epoch(
     optimizer.zero_grad()
     total_samples = len(loader.dataset)
     for batch_idx, (input, target) in enumerate(loader):
+        if args.num_classes == 1000 and batch_idx % 2==1:
+            continue
+        if batch_idx>6000 and args.num_classes == 1000:
+            total_samples = 3000
+            break
         last_batch = batch_idx == last_idx
         data_time_m.update(time.time() - end)
         if not args.prefetcher:
@@ -878,9 +883,7 @@ def train_one_epoch(
         if args.num_classes == 1000 and batch_idx % 50==0:
             _logger.info("batch_idx:"+str(batch_idx))
         # for imagenet, number of samples are 2000
-        if batch_idx>2000 and args.num_classes == 1000:
-            total_samples = 2000
-            break
+        
         # end for
     
     if hasattr(optimizer, 'sync_lookahead'):
@@ -891,6 +894,70 @@ def train_one_epoch(
             param.grad.data /= total_samples
             # _logger.info("mean grad:"+str(torch.mean(param.grad.data)))
     return OrderedDict([('loss', losses_m.avg)])
+
+def validate(model, loader, loss_fn, args, amp_autocast=suppress, log_suffix=''):
+    batch_time_m = AverageMeter()
+    losses_m = AverageMeter()
+    top1_m = AverageMeter()
+    top5_m = AverageMeter()
+
+    model.eval()
+
+    end = time.time()
+    last_idx = len(loader) - 1
+    with torch.no_grad():
+        for batch_idx, (input, target) in enumerate(loader):
+            last_batch = batch_idx == last_idx
+            if not args.prefetcher:
+                input = input.cuda()
+                target = target.cuda()
+            if args.channels_last:
+                input = input.contiguous(memory_format=torch.channels_last)
+
+            with amp_autocast():
+                output = model(input)
+            if isinstance(output, (tuple, list)):
+                output = output[0]
+
+            # augmentation reduction
+            reduce_factor = args.tta
+            if reduce_factor > 1:
+                output = output.unfold(0, reduce_factor, reduce_factor).mean(dim=2)
+                target = target[0:target.size(0):reduce_factor]
+
+            loss = loss_fn(output, target)
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
+            if args.distributed:
+                reduced_loss = reduce_tensor(loss.data, args.world_size)
+                acc1 = reduce_tensor(acc1, args.world_size)
+                acc5 = reduce_tensor(acc5, args.world_size)
+            else:
+                reduced_loss = loss.data
+
+            torch.cuda.synchronize()
+
+            losses_m.update(reduced_loss.item(), input.size(0))
+            top1_m.update(acc1.item(), output.size(0))
+            top5_m.update(acc5.item(), output.size(0))
+
+            batch_time_m.update(time.time() - end)
+            end = time.time()
+            if args.local_rank == 0 and (last_batch or batch_idx % args.log_interval == 0):
+                log_name = 'Test' + log_suffix
+                _logger.info(
+                    '{0}: [{1:>4d}/{2}]  '
+                    'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})  '
+                    'Loss: {loss.val:>7.4f} ({loss.avg:>6.4f})  '
+                    'Acc@1: {top1.val:>7.4f} ({top1.avg:>7.4f})  '
+                    'Acc@5: {top5.val:>7.4f} ({top5.avg:>7.4f})'.format(
+                        log_name, batch_idx, last_idx, batch_time=batch_time_m,
+                        loss=losses_m, top1=top1_m, top5=top5_m))
+
+    metrics = OrderedDict([('loss', losses_m.avg), ('top1', top1_m.avg), ('top5', top5_m.avg)])
+
+    return metrics
+
 
 
 if __name__ == '__main__':
