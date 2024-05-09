caa166923572940beb1d13363718a3b479bb6d1b
1907FB <994876761@qq.com>
2024-05-09 12:25:35 +0800

    prune resnet

--- train_prune.sh
+++ train_prune.sh
@@ -13,8 +13,8 @@ CUDA_VISIBLE_DEVICES=6 python train_prune.py -c configs/datasets/Prune/ViT/cifar
 CUDA_VISIBLE_DEVICES=6 python train_prune.py -c configs/datasets/Prune/ViT/tiny.yml --model vit_9_12_64 /home/xts/code/dataset/tiny-imagenet-200/
 
 # cvt SpENCNN, pruning
-CUDA_VISIBLE_DEVICES=5 python train_prune.py -c configs/datasets/Prune/CVT/cifar10.yml --model cvt_7_4_32 /home/xts/code/dataset/cifar10/
+CUDA_VISIBLE_DEVICES=1 python train_prune.py -c configs/datasets/Prune/CVT/cifar10.yml --model cvt_7_4_32 /home/xts/code/dataset/cifar10/
 
-CUDA_VISIBLE_DEVICES=5 python train_prune.py -c configs/datasets/Prune/CVT/cifar100.yml --model cvt_7_4_32_c100 /home/xts/code/dataset/cifar100/
+CUDA_VISIBLE_DEVICES=2 python train_prune.py -c configs/datasets/Prune/CVT/cifar100.yml --model cvt_7_4_32_c100 /home/xts/code/dataset/cifar100/
 
-CUDA_VISIBLE_DEVICES=6 python train_prune.py -c configs/datasets/Prune/CVT/tiny.yml --model cvt_9_12_64 /home/xts/code/dataset/tiny-imagenet-200/
+CUDA_VISIBLE_DEVICES=3 python train_prune.py -c configs/datasets/Prune/CVT/tiny.yml --model cvt_9_12_64 /home/xts/code/dataset/tiny-imagenet-200/
