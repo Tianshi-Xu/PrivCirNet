cb370fdf855e2f052e01f1cbf77b2f0cf7e15622
1907FB <994876761@qq.com>
2024-05-13 09:38:20 +0800

    imagenet config

--- ILP.sh
+++ ILP.sh
@@ -2,12 +2,12 @@
 CUDA_VISIBLE_DEVICES=2 python CirILP.py -c configs/datasets/MBV2/cifar10_ILP.yml --model c10_cir_mobilenetv2 /home/xts/code/dataset/cifar10/
 CUDA_VISIBLE_DEVICES=6 python CirILP.py -c configs/datasets/MBV2/cifar100_ILP.yml --model c100_cir_mobilenetv2 /home/xts/code/dataset/cifar100/
 CUDA_VISIBLE_DEVICES=5 python CirILP.py -c configs/datasets/MBV2/tiny_ILP.yml --model tiny_cir_mobilenetv2 /home/xts/code/dataset/tiny-imagenet-200/
-CUDA_VISIBLE_DEVICES=7 python CirILP.py -c configs/datasets/MBV2/imagenet_ILP.yml --model image_cir_mobilenetv2 /opt/dataset/imagenet/
+CUDA_VISIBLE_DEVICES=4 python CirILP.py -c configs/datasets/MBV2/imagenet_ILP.yml --model image_cir_mobilenetv2 /opt/dataset/imagenet/
 
 # resnet ILP
-CUDA_VISIBLE_DEVICES=4 python CirILP.py -c configs/datasets/ResNet/cifar10_ILP.yml --model cir_cifar10_resnet18 /home/xts/code/dataset/cifar10/
-CUDA_VISIBLE_DEVICES=5 python CirILP.py -c configs/datasets/ResNet/cifar100_ILP.yml --model cir_cifar100_resnet18 /home/xts/code/dataset/cifar100/
-CUDA_VISIBLE_DEVICES=6 python CirILP.py -c configs/datasets/ResNet/tiny_ILP.yml --model cir_tiny_resnet18 /home/xts/code/dataset/tiny-imagenet-200/
+CUDA_VISIBLE_DEVICES=1 python CirILP.py -c configs/datasets/ResNet/cifar10_ILP.yml --model cir_cifar10_resnet18 /home/xts/code/dataset/cifar10/
+CUDA_VISIBLE_DEVICES=2 python CirILP.py -c configs/datasets/ResNet/cifar100_ILP.yml --model cir_cifar100_resnet18 /home/xts/code/dataset/cifar100/
+CUDA_VISIBLE_DEVICES=3 python CirILP.py -c configs/datasets/ResNet/tiny_ILP.yml --model cir_tiny_resnet18 /home/xts/code/dataset/tiny-imagenet-200/
 
 # vit ILP
 CUDA_VISIBLE_DEVICES=6 python CirILP.py -c configs/datasets/ViT/cifar10_ILP.yml --model vit_7_4_32 /home/xts/code/dataset/cifar10/
